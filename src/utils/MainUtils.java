053833662cbf1675673719216a458af817286917
valeriiav <valeriia.verbytska@gmail.com>
2024-01-30 12:41:12 -0600

    Fix typo MainUtils.java

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -7,6 +7,6 @@ public class MainUtils {
     // method 2
 
     public static void main(String[] args) {
-        System.out.println("thi sis from Celine branch");
+        System.out.println("this is from Celine branch");
     }
 }
