b50a5daf30bd8ea217fd040e7228627fe7d5fc93
valeriiav <valeriia.verbytska@gmail.com>
2024-01-30 13:09:43 -0600

    update project for future tasks

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -2,17 +2,4 @@ package utils;
 
 public class MainUtils {
 
-    // method 1
-
-    // method 2
-
-    public static void main(String[] args) {
-
-        System.out.println("thi sis from Celine branch");
-        System.out.println("Celine 2");
-
-        System.out.println("this is from Celine branch");
-        System.out.println("Valerria print statement");
-
-    }
 }
--- src/week1_Celine/OddOrEven.java
+++ src/week1_Celine/OddOrEven.java
@@ -1,4 +1,6 @@
 package week1_Celine;
 
 public class OddOrEven {
+    //EXAMPLE PACKAGE AND CLASS FOR OUR JAVA TASKS
+    //please use this convention to create your own
 }
