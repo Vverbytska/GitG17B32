b344a94e1b5bef22fcfbc610364e463784d61fcd
linaalrawi <linaessam81@gmail.com>
2024-03-03 21:15:52 -0600

    I Just added @Lina week4 updated 2

---
+++ src/week5_Lina/ReverseString.java
@@ -0,0 +1,21 @@
+package week5_Lina;
+
+public class ReverseString {
+    public static String reverse(String input) {
+
+        // create an empty string to store the reversed string
+        StringBuilder sb = new StringBuilder();
+
+        // loop through each character in the input string in reverse order
+        for (int i = input.length() - 1; i >= 0; i--) {
+            sb.append(input.charAt(i));
+        }
+
+        // Return the reversed string
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("ABCD")); // Output: DCBA
+    }
+}
---
+++ src/week5_Lina/UniqueCharacters.java
@@ -0,0 +1,25 @@
+package week5_Lina;
+
+public class UniqueCharacters {
+    public static String unique(String input) {
+        // create an empty string to store the unique characters
+        StringBuilder sb = new StringBuilder();
+
+        // loop through each character in the input string
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            // only adding the character to the string of unique characters if it doesn't already exist
+            if (c != 'A' && c != 'B' && c != 'C' && sb.indexOf("" + c) == -1) {
+                sb.append(c);
+            }
+        }
+
+        // returning the string of unique characters
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(unique("AAABBBCCCDEF")); // Output: "DEF"
+    }
+}
