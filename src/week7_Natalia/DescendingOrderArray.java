f7a63f90d910cdfe413b292d184df332921d7625
Natalia <rozdobudkonatalya1@gmail.com>
2024-03-27 16:23:18 -0500

    Update DescendingOrderArray

--- src/week7_Natalia/DescendingOrderArray.java
+++ src/week7_Natalia/DescendingOrderArray.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class DescendingOrderArray {
     public static void main(String[] args) {
         int[] array = {34, 78, 1, 7, 98, -2};
-        System.out.println("sortAscending(array) = " + Arrays.toString(sortAscending(array)));
+        System.out.println("sortDescending(array) = " + Arrays.toString(sortDescending(array)));
     }
 
-    public static int[] sortAscending(int[] array) {
+    public static int[] sortDescending(int[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if (array[i] > array[j]) {
